92ceb73e792de5533d7ae861bcecc7559f0433c2
Alasdair Wem <alasdair123@hotmail.co.uk>
2019-04-03 15:45:11 +0100

    docking station gets bike and checks working status

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,6 +2,25 @@ require 'docking_station'
 
 describe DockingStation do
   it "respond to release_bike" do
-    expect(subject).to respond_to :release_bike  
+    expect(subject).to respond_to :release_bike
   end
-end
+
+  it "releases working bikes" do
+    bike = subject.release_bike
+    expect(bike).to be_working
+  end
+
+  it 'docks something' do
+    bike = Bike.new
+    expect(subject.dock(bike)).to eq bike
+  end
+
+  it { is_expected.to respond_to(:bike) }
+
+  it "returns docked bikes" do
+    bike = Bike.new
+    subject.dock(bike)
+    expect(subject.bike).to eq bike
+  end
+
+  end
